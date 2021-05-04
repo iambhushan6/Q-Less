18441f9df3c431eccbf1be3e09736a306a79de14
Bhushan Lokhande <bhushan.lokhande19@vit.edu>
2021-05-04 23:34:47 +0530

    Delete Tokenizer directory

--- Tokenizer/Tokenapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Tokenapp.models import Student
-
-# Register your models here.
-admin.site.register(Student)
--- Tokenizer/Tokenapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TokenappConfig(AppConfig):
-    name = 'Tokenapp'
